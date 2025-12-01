687d80c74971f532f475c4bd07d6af4074851e73
BiellNonato <gabrielnonato53@gmail.com>
2025-12-01 16:52:07 -0300

    feat: login input

--- src/components/layouts/Login/index.jsx
+++ src/components/layouts/Login/index.jsx
@@ -3,20 +3,26 @@ import './style.css'
 export const Login = () => {
     return (
         <>
-            <section className='Login-container'>
-                <header className='Login-content'>
-                    <img src={Opalao} alt="" />
-                    <div className="Login-info">
-                        <h1>ParkingZ</h1>
-                        <p>Login</p>
+            <section className="Login">
+                <article className='Login-container'>
+                    <header className='Login-content'>
+                        <img src={Opalao} alt="" />
+                        <div className="Login-info">
+                            <h1>ParkingZ</h1>
+                            <p>Login</p>
+                        </div>
+                    </header>
+                    <article className="login-inputs">
+                        <input type="text" placeholder="Email:" maxLength={30} required />
+                        <input type="password" placeholder="Senha:" maxLength={10} required />
+                        <button type="submit"></button>
+                    </article>
+                    <div className="register-line">
+                        <p>Não tem login? <span>cadastre-se</span> </p>
                     </div>
-                </header>
-                <article className="login-inputs">
-                    <input type="text" placeholder="Email" />
-                    <input type="password" placeholder="Senha" />
-                    <button type="submit"></button>
                 </article>
             </section>
+
 
         </>
     )
